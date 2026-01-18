e7ab820df97cb2caae4bd1dfe659521fd40544b5
VISHALSK03 <145835599+VISHALSK03@users.noreply.github.com>
2026-01-18 21:49:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (32.41%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.c
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -1,11 +1,16 @@
-int reverse(int x){
-    int d=0;
-    int r=0;
-    while(x!=0){
-        d=x%10;
-        r=r*10+d;
-        x=x/10;
+int reverse(int x) {
+    int r = 0;
+
+    while (x != 0) {
+        int d = x % 10;
+        x /= 10;
+
+        if (r > INT_MAX / 10 || (r == INT_MAX / 10 && d > 7))
+            return 0;
+        if (r < INT_MIN / 10 || (r == INT_MIN / 10 && d < -8))
+            return 0;
+
+        r = r * 10 + d;
     }
     return r;
-
 }
